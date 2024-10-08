713022467fd99bce75ca702e39833b8549043dd9
jnether814 <jnethercott0814@gmail.com>
2024-10-07 18:25:09 -0700

    task 1 complete

---
+++ task1_lists.py
@@ -0,0 +1,24 @@
+#Create a list of movie titles, ratings, and genres:
+#Example: movies = ['Inception', 'The Dark Knight', 'Interstellar']
+#Example: ratings = [9, 8.5, 9.5]
+#Example: genres = ['Sci-Fi', 'Action', 'Drama']
+
+movies = ['The Emoji Movie', 'Inside Out', 'The Super Mario Movie']
+ratings = [3.4, 8.1, 7.0]
+genres = ['Comedy', 'Drama', 'Comedy']
+
+#Access and modify items in the list:
+#Print the first movie in the list: print(movies[0])
+#Change the rating of 'Inception' to 9.2: ratings[0] = 9.2
+
+print(movies[0])
+ratings[0] = 5.2
+print(ratings[0])
+
+#Add new movies to the list:
+#Use the append() function to add a new movie to the list:
+#movies.append('Tenet')
+#ratings.append(7.5)
+#genres.append('Thriller')
+
+
